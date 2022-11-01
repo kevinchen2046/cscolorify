c585f4d7fb9719672e4fb7b4b14e2b7e44947006
nan.chen <kevin-chen@foxmail.com>
2022-11-01 11:50:47 +0800

    x
    x

--- index.js
+++ index.js
@@ -1,73 +1,43 @@
 "use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
 let $console = window.console;
-/**
- * Console 多彩版
- * - 支持chrome浏览器多彩显示,其他浏览器不明确(未测试)
- * - 提供style接口,可自定义样式。
- * - 目前样式只支持 bgcolor: 背景颜色, color: 文字颜色, size:字号
- * - 各接口颜色一览
- *   - log 灰色
- *   - info 绿色
- *   - warn 橘色
- *   - error 红色
- *   - debug 蓝色
- *   - table 紫色
- * @author kevin.chen
- * @email kevin-chen@foxmail.com
- * @date 2022/9/5
- */
 class Cscolorify {
     constructor() {
         this.enable = true;
         this.gap = true;
         this.__counts = {};
     }
-    /**灰色 */
     log(...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.log, 0x333333, 0xcecece, ['LOG:', ...args]);
+        this.__outLog($console.log, 0x333333, 0xcecece, ['[LOG]', ...args]);
     }
-    /**绿色 */
     info(...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.log, 0x2C6D1B, 0xCEFDCE, ['INFO:', ...args]);
+        this.__outLog($console.log, 0x2C6D1B, 0xCEFDCE, ['[INFO]', ...args]);
     }
-    /**橘色 */
     warn(...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.warn, 0x885200, 0xFDEACE, ['WARN:', ...args]);
+        this.__outLog($console.warn, 0x885200, 0xFDEACE, ['[WARN]', ...args]);
     }
-    /**蓝色 */
     debug(...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.log, 0x09677F, 0xCEEAFD, ['DEBUG:', ...args]);
+        this.__outLog($console.log, 0x09677F, 0xCEEAFD, ['[DEBUG]', ...args]);
     }
-    /**红色 */
     error(...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.error, 0x840505, 0xFDCECE, ['ERROR:', ...args]);
+        this.__outLog($console.error, 0x840505, 0xFDCECE, ['[ERROR]', ...args]);
     }
-    /**紫色 */
     table(tabularData, properties, ...args) {
         if (!this.enable)
             return;
-        this.__outLog($console.group, 0x520088, 0xEBCCFF, [`TABLE: ${args.join(" ")}`]);
+        this.__outLog($console.group, 0x520088, 0xEBCCFF, [`[TABLE] ${args.join(" ")}`]);
         $console.table(tabularData, properties);
         $console.groupEnd();
     }
-    /**
-     * 断言
-     * - 请注意与原生`console.assert`用法不同
-     * @param expression 当表达式为true时触发断言
-     * @param args
-     * @returns
-     */
     assert(expression, ...args) {
         if (!this.enable)
             return;
@@ -79,7 +49,7 @@ class Cscolorify {
         if (this.__counts[key] === undefined)
             this.__counts[key] = 0;
         let value = ++this.__counts[key];
-        this.__outLog($console.log, 0x09677F, 0xCEEAFD, ['COUNT:', label, value]);
+        this.__outLog($console.log, 0x09677F, 0xCEEAFD, ['[COUNT]', label, value]);
     }
     countReset(label) {
         let key = label ? label : "undefined";
@@ -99,15 +69,9 @@ class Cscolorify {
     timeLog(label, ...data) { $console.timeLog(label, ...data); }
     timeStamp(label) { $console.timeStamp(label); }
     trace(...data) { $console.trace(...data); }
-    /**
-     * 关闭控制台
-     * - 用户将无法打开 开发者工具
-     * - 该方法适用于发行版本
-     */
     disable() {
         this.enable = false;
     }
-    /**自定义 */
     style(...args) {
         if (!this.enable)
             return;
